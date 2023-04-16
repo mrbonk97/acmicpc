c619a011cab140e28531a394b3ee95212423eaff
hyunsuk97 <mrbonk97@gmail.com>
2023-04-17 00:59:23 +0900

    10998 해결

--- S1000.js
+++ S1000.js
@@ -1,5 +1,5 @@
 let fs = require('fs');
-let input = fs.readFileSync('dev/stdin').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let line = input[0].split(' ');
 
